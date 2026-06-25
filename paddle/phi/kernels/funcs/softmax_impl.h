b4971e960e758be5eb724e085cecbad5f26a98fb
PrernaKumbhar20 <Prerna.Kumbhar@ibm.com>
2026-06-25 14:32:39 +0530

    forth commit

--- paddle/phi/kernels/funcs/softmax_impl.h
+++ paddle/phi/kernels/funcs/softmax_impl.h
@@ -242,10 +242,10 @@ class SoftmaxFunctor<DeviceContext, T, enable_if_CPU<DeviceContext>> {
     constexpr int kClassDim = 1;
 
     const int num_classes = in_dims[kClassDim];
+#if defined(__AVX__) && !defined(__powerpc__) && !defined(__ppc__) && !defined(__PPC__)
     const int batch_size = in_dims[kBatchDim];
     const int num_remain = num_classes / axis_dim;
 
-#if defined(__AVX__) && !defined(__powerpc__) && !defined(__ppc__) && !defined(__PPC__)
     if (num_remain == 1 &&
         phi::backends::cpu::MayIUse(phi::backends::cpu::avx)) {
       const T* in_data = X->data<T>();
@@ -401,10 +401,10 @@ class SoftmaxGradFunctor<DeviceContext, T, enable_if_CPU<DeviceContext>> {
     constexpr int kBatchDim = 0;
     constexpr int kClassDim = 1;
     const int num_classes = out_dims[kClassDim];
+#if defined(__AVX__) && !defined(__powerpc__) && !defined(__ppc__) && !defined(__PPC__)
     const int batch_size = out_dims[kBatchDim];
     const int num_remain = num_classes / axis_dim;
 
-#if defined(__AVX__) && !defined(__powerpc__) && !defined(__ppc__) && !defined(__PPC__)
     if (num_remain == 1 &&
         phi::backends::cpu::MayIUse(phi::backends::cpu::avx)) {
       const T* out_data = y->data<T>();
