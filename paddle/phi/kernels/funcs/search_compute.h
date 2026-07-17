2d12dedd576e6071355e0a10b91aa3dba619c215
root <root@prerna-kumbhar.pokprv.stglabs.ibm.com>
2026-07-17 04:20:26 -0400

    Fixed ppc64le build by excluding SIMD/AVX code paths

--- paddle/phi/kernels/funcs/search_compute.h
+++ paddle/phi/kernels/funcs/search_compute.h
@@ -15,7 +15,8 @@
 #pragma once
 
 #if !defined(PADDLE_WITH_ARM) && !defined(PADDLE_WITH_SW) && \
-    !defined(PADDLE_WITH_MIPS) && !defined(PADDLE_WITH_LOONGARCH)
+    !defined(PADDLE_WITH_MIPS) && !defined(PADDLE_WITH_LOONGARCH) && \
+    !defined(__powerpc__) && !defined(__ppc__) && !defined(__PPC__)
 #include <immintrin.h>
 #endif
 #include <cfloat>
@@ -101,7 +102,8 @@ void call_gemm_batched(const Context& dev_ctx,
 }
 
 #if !defined(PADDLE_WITH_ARM) && !defined(PADDLE_WITH_SW) && \
-    !defined(PADDLE_WITH_MIPS) && !defined(PADDLE_WITH_LOONGARCH)
+    !defined(PADDLE_WITH_MIPS) && !defined(PADDLE_WITH_LOONGARCH) && \
+    !defined(__powerpc__) && !defined(__ppc__) && !defined(__PPC__)
 
 #define __m256x __m256
 
@@ -142,7 +144,8 @@ inline void axpy(const T* x, T* y, size_t len, const T alpha) {
                       _mm256_mul_px(mm_alpha, _mm256_load_px(x + jjj))));
   }
 #elif defined(PADDLE_WITH_ARM) || defined(PADDLE_WITH_SW) || \
-    defined(PADDLE_WITH_MIPS) || defined(PADDLE_WITH_LOONGARCH)
+    defined(PADDLE_WITH_MIPS) || defined(PADDLE_WITH_LOONGARCH) || \
+    defined(__powerpc__) || defined(__ppc__) || defined(__PPC__)
   PADDLE_THROW(common::errors::Unimplemented("axpy is not supported"));
 #else
   lll = len & ~SSE_CUT_LEN_MASK;
@@ -172,7 +175,8 @@ inline void axpy_noadd(const T* x, T* y, size_t len, const T alpha) {
     _mm256_store_px(y + jjj, _mm256_mul_px(mm_alpha, _mm256_load_px(x + jjj)));
   }
 #elif defined(PADDLE_WITH_ARM) || defined(PADDLE_WITH_SW) || \
-    defined(PADDLE_WITH_MIPS) || defined(PADDLE_WITH_LOONGARCH)
+    defined(PADDLE_WITH_MIPS) || defined(PADDLE_WITH_LOONGARCH) || \
+    defined(__powerpc__) || defined(__ppc__) || defined(__PPC__) 
   PADDLE_THROW(common::errors::Unimplemented("axpy_noadd is not supported"));
 #else
   lll = len & ~SSE_CUT_LEN_MASK;
